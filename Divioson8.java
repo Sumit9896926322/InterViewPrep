b32864d4fa50bea8d944a92ae4b395da5456ac93
Sumit Singh <writeonsumit@gmail.com>
2020-03-24 23:39:11 +0530

    4 problems

--- Divioson8.java
+++ Divioson8.java
@@ -3,6 +3,8 @@ package GFG;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class Divioson8 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -32,10 +34,40 @@ public class Divioson8 {
 
 		int T = nextInt();
 		while (T-- > 0) {
+			String arr[] = next().split("");
+			ArrayList<Integer> res = new ArrayList<Integer>();
+			StringBuilder str = new StringBuilder();
+			String last = "";
+			Boolean flag = false;
+			BigInteger eight = new BigInteger("8");
 
-			int n = nextInt();
-			int arr[] = nextArr();
+			for (int i = 0; i < arr.length; i++) {
+				res.add(Integer.parseInt(arr[i]));
+				str.append(arr[i]);
+			}
 
+			q: for (int k = 0; k < arr.length; k++) {
+				for (int i = 0; i < res.size(); i++) {
+					for (int j = 0; j < res.size(); j++) {
+						if (j == i || j == k || i == k)
+							continue;
+
+						BigInteger number = new BigInteger(res.get(i) + "" + res.get(j) + "" + res.get(k));
+						if (number.remainder(eight).equals(BigInteger.ZERO)) {
+							flag = true;
+							break q;
+						}
+					}
+				}
+
+			}
+			// for strings length equal to 2
+			if (new BigInteger(str.toString()).remainder(eight).equals(BigInteger.ZERO)
+					|| new BigInteger(str.reverse().toString()).remainder(eight).equals(BigInteger.ZERO)) {
+				System.out.println("YES");
+				continue;
+			}
+			System.out.println(flag ? "YES" : "NO");
 		}
 	}
 
