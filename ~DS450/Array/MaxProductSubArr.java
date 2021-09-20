bb155a5f6e8e60c3d56466be83256a317007d3eb
sumit <sumit@bitcs.in>
2021-09-20 23:38:30 +0530

    clean up

--- ~DS450/Array/MaxProductSubArr.java
+++
@@ -1,8 +0,0 @@
-package Array;
-import java.util.*;
-public class MaxProductSubArr {
-    Scanner s = new Scanner(System.in);
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
